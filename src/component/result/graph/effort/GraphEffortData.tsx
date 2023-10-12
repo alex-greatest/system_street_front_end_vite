f248e9f82dca0caded687c63f81bf437664b9799
rena <ssss1231@yande.ru>
2023-10-12 21:03:11 +0400

    Переходим к подготовке графиков

--- src/component/result/graph/effort/GraphEffortData.tsx
+++ src/component/result/graph/effort/GraphEffortData.tsx
@@ -22,6 +22,8 @@ export const GraphEffortData = observer((props:
         isSuccess
     } = useGetGraphResultEffort(isNaN(+operationId) ? -1 : +operationId);
 
+    console.log(data?.pointsGraphRight);
+
     const dataMemo = useMemo(() => data, [data]);
 
     if (isError || (isSuccess && (!data || !data.pointsGraphLeft || data.pointsGraphLeft.length === 0))) {
--- src/component/result/graph/effort/LineChartGraphEffort.tsx
+++ src/component/result/graph/effort/LineChartGraphEffort.tsx
@@ -46,8 +46,45 @@ export const LineChartGraphEffort = observer((props:
                 <CartesianGrid horizontal={true} vertical={true} />
                 <XAxis
                     type="number"
-                    domain={[(dataMin: number) => Math.round(dataMin),
-                        (dataMax: number) => Math.round(dataMax)]}
+                    xAxisId={"right_effort_x"}
+                    dataKey="x">
+                    <Label style={{fill: 'blue', fontSize: '25px',}} offset={-100} dy={25} position="center" >
+                        Ход рейки, мм
+                    </Label>
+                </XAxis>
+                <YAxis type="number"
+                       yAxisId={"right_effort_y"}
+                       dataKey={"y"}>
+                    <Label style={{fill: 'blue', fontSize: '25px',}} angle={-90} dx={-20} position="center" >
+                        Усилие преремещения рейки, Н
+                    </Label>
+                </YAxis>
+                <Legend verticalAlign="top"/>
+                <Line
+                    onAnimationEnd={() => {setIsLoadSecondLine(true)}}
+                    xAxisId={"right_effort_x"}
+                    yAxisId={"right_effort_y"}
+                    name="Усилие справа"
+                    data={data?.pointsGraphRight}
+                    type="monotone"
+                    dataKey="y"
+                    stroke="#0000FF"
+                    dot={false}
+                />
+            </LineChart>
+            {/*<LineChart
+                key={"lineChartGraphEffort"}
+                width={500}
+                height={300}
+                margin={{
+                    top: 5,
+                    right: 25,
+                    left: 20,
+                    bottom: 25
+                }}>
+                <CartesianGrid horizontal={true} vertical={true} />
+                <XAxis
+                    type="number"
                     dataKey="x">
                     <Label style={{fill: 'blue', fontSize: '25px',}} offset={-100} dy={25} position="center" >
                         Ход рейки, мм
@@ -55,9 +92,7 @@ export const LineChartGraphEffort = observer((props:
                 </XAxis>
                 <YAxis type="number"
                        tickCount={8} interval={0}
-                       dataKey={"y"}
-                       domain={[(dataMin: number) => Math.round((dataMin * 0.33) + dataMin),
-                           (dataMax: number) => Math.round((dataMax * 0.33) + dataMax)]}>
+                       dataKey={"y"}>
                     <Label style={{fill: 'blue', fontSize: '25px',}} angle={-90} dx={-20} position="center" >
                         Усилие преремещения рейки, Н
                     </Label>
@@ -86,7 +121,7 @@ export const LineChartGraphEffort = observer((props:
                 <Line name="Среднее значение слева" strokeDasharray="1 3" dataKey="y" stroke="red" />
                 <ReferenceLine strokeDasharray="15 3" name={"Среднее значение справа"} y={data?.cwAvg} stroke="green" />
                 <ReferenceLine strokeDasharray="10 7" name={"Среднее значение слева"} y={data?.cwwAvg} stroke="red" />
-            </LineChart>
+            </LineChart>*/}
         </ResponsiveContainer>
     );
 });
